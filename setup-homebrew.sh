07408e30f690d366cc4583594242e6f536c94ad2
Steven Landow <landow@google.com>
2022-11-02 09:11:19 -0700

    fix ignore homebrew on linux

--- setup-homebrew.sh
+++ setup-homebrew.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-if [ "$(uname)" != "Darwin" ]; then
-	echo "not a mac" && exit 0
-fi
+function setup_brew() {
+	if [ "$(uname)" != "Darwin" ]; then
+		echo "not a mac" && return 0
+	fi
 
-# Check if Homebrew is installed
-if which brew; then
-  echo 'Updating homebrew'
-  brew update
-else
-	echo "Installing Homebrew"
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+	# Check if Homebrew is installed
+	if which brew; then
+	  echo 'Updating homebrew'
+	  brew update
+	else
+		echo "Installing Homebrew"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+}
+setup_brew
 
