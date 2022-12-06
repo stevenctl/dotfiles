a700969f8144edff0092776ff749580a66269cb3
Steven Landow <landow@google.com>
2022-12-06 23:35:40 +0000

    skip gcloud step

--- zsh/oh-my-zsh-custom/1.gcp.zsh
+++ zsh/oh-my-zsh-custom/1.gcp.zsh
@@ -5,6 +5,6 @@ if [ -f '/home/landow/google-cloud-sdk/path.zsh.inc' ]; then . '/home/landow/goo
 if [ -f '/home/landow/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/landow/google-cloud-sdk/completion.zsh.inc'; fi
 
 # This may be annoying in some environments, but for most of my work I need it. 
-export GCP_CREDENTIALS="${HOME}/.config/gcloud/application_default_credentials.json"
-if which gcloud > /dev/null && [ ! -f "${GCP_CREDENTIALS}" ]; then gcloud auth application-default login; fi
+# export GCP_CREDENTIALS="${HOME}/.config/gcloud/application_default_credentials.json"
+# if which gcloud > /dev/null && [ ! -f "${GCP_CREDENTIALS}" ]; then gcloud auth application-default login; fi
 
