3176657ba1a5066a5dcd7d2143f8c5e6ea70dfe9
Steven Landow <landow@google.com>
2022-12-19 15:19:49 -0800

    fix mapping

--- editors/neovim/lua/custom/plugins/mason.lua
+++ editors/neovim/lua/custom/plugins/mason.lua
@@ -14,5 +14,7 @@ return {
     "python-lsp-server",
     "debugpy",
     "black",
+
+    "markdownlint",
   },
 }
--- editors/neovim/lua/custom/plugins/null-ls.lua
+++ editors/neovim/lua/custom/plugins/null-ls.lua
@@ -5,11 +5,11 @@ end
 
 -- Enables anything null-ls related that was installed via mason
 local mason_items = require("core.utils").load_override({}, "williamboman/mason.nvim").ensure_installed
-local mapping = require("mason-null-ls.mappings.source").package_to_null_ls
+local mapping = require("mason-null-ls.mappings.source").getNullLsFromPackage
 local source_types = { "formatting", "diagnostics", "hover", "completion", "code_actions" }
 local sources = {}
 for _, package_name in ipairs(mason_items) do
-  local source_name = mapping[package_name]
+  local source_name = mapping(package_name)
   if source_name then
     for _, srcs in ipairs(source_types) do
       local source_ok, source = pcall(require, "null-ls.builtins." .. srcs .. "." .. source_name)
