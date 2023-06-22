12eb53d944b7fc3dc2ed96dbcd39d090f091e00c
Steven Landow <steven.landow@solo.io>
2023-06-22 21:58:21 +0000

    dont grab nerdfont

--- setup-neovim.sh
+++ setup-neovim.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-curl -sS https://webi.sh/nerdfont | sh
 
