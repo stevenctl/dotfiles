23145ba56c2cb1fee17bbf6f57cc8f796c2d3235
Steven Landow <steven@landow.dev>
2023-09-15 10:05:44 -0700

    scope oldfiles to cwd

--- editors/lvim/lua/user/plugins/telescope.lua
+++ editors/lvim/lua/user/plugins/telescope.lua
@@ -3,6 +3,9 @@ vim.list_extend(lvim.plugins, {
   "nvim-telescope/telescope-live-grep-args.nvim",
 })
 -- lvim.builtin.telescope.defaults.pickers.colorscheme = { enable_preview = true } -- TODO this doesn't work
+
+lvim.builtin.telescope.pickers.oldfiles = { cwd_only = true }
+
 lvim.builtin.telescope.on_config_done = function()
   require("telescope").load_extension "live_grep_args"
 end
