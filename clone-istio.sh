f41866cff82b9eb1cf7fd5e415bf6d425aff2329
Steven Landow <steven@landow.dev>
2023-12-03 00:19:10 -0800

    clone proxy

--- clone-istio.sh
+++ clone-istio.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $HOME/go/src/istio.io
 pushd $HOME/go/src/istio.io
-for repo in istio ztunnel test-infra common-files; do
+for repo in istio ztunnel test-infra common-files proxy; do
   git clone git@github.com:istio/$repo
   pushd $repo
   git remote add fork git@github.com:stevenctl/$repo
