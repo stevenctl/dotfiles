e8954a136c57c34bcc91331dd27a14a4ddc68631
Steven Landow <steven@landow.dev>
2024-04-11 11:37:33 -0700

    polybar and pinyin changes

--- wm/polybar.sh
+++ wm/polybar.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
+if pgrep -x polybar >/dev/null; then
+  # Terminate already running bar instances
+  killall -q polybar
+  # Wait until the processes have been shut down
+  while pgrep -x polybar >/dev/null; do sleep 1; done
+else
+  # Terminate already running bar instances
+  killall -q polybar
+  # Wait until the processes have been shut down
+  while pgrep -x polybar >/dev/null; do sleep 1; done
+  # Launch polybar
+  polybar example &
+fi
 
-# Terminate already running bar instances
-killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar example &
