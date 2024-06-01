111d0bd80d8ffc26cec35d151c137ddc5ba13de5
Steven Landow <steven.landow@solo.io>
2024-05-31 18:48:06 -0700

    switch to non-lunar nvim

--- zsh/oh-my-zsh-custom/0.editor.zsh
+++ zsh/oh-my-zsh-custom/0.editor.zsh
@@ -1,6 +1,7 @@
 export PATH="$PATH:/usr/bin/nvim-linux64/bin"
-export EDITOR=lvim
-export VISUAL=lvim
-alias vi=lvim
-alias vim=lvim
+export EDITOR=nvim
+export VISUAL=nvim
+alias vi=nvim
+alias vim=nvim
+alias lvim=nvim
 
