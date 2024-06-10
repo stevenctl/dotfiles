5be00a3ba4e25531d93795c961a0ee771583c7e3
Steven Landow <steven@landow.dev>
2024-06-10 05:33:04 -0700

    q to close

--- editors/nvim/lua/user/plugins/bqf.lua
+++ editors/nvim/lua/user/plugins/bqf.lua
@@ -1,3 +1,21 @@
+-- Easyclose for a lot of things
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"checkhealth",
+		"fugitive*",
+		"git",
+		"help",
+		"lspinfo",
+		"netrw",
+		"notify",
+		"qf",
+		"query",
+	},
+	callback = function()
+		vim.keymap.set("n", "q", vim.cmd.close, { desc = "Close the current buffer", buffer = true })
+	end,
+})
+
 return {
 	"kevinhwang91/nvim-bqf",
 	event = { "BufRead", "BufNew" },
