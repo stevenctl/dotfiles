a80c74dbe420fd534fdb1df03eb5b3e633660a86
Steven Landow <steven@landow.dev>
2025-06-12 10:39:46 -0700

    touchups

--- zsh/oh-my-zsh-custom/2.istio.zsh
+++ zsh/oh-my-zsh-custom/2.istio.zsh
@@ -4,8 +4,6 @@ export ZTUNNEL=$ISTIO/../ztunnel
 export ISTIO_VERSION=1.25.0
 export ISTIO_DIR="$HOME/istio-${ISTIO_VERSION}"
 export PATH="${ISTIO_DIR}/bin:$PATH"
-export PATH="$PATH:$HOME/.gloo-mesh/bin"
-export PATH=$HOME/.gloo/bin:$PATH
 export ENVOY_DOCKER_OPTIONS="-v $GCP_CREDENTIALS:$GCP_CREDENTIALS"
 
 function download_istio_bin() {
@@ -15,31 +13,23 @@ function download_istio_bin() {
 	popd
 }
 
-function istio_build_core() {
+function istio_build_docker_push() {
 	if [ "$(pwd)" != "$ISTIO" ]; then
 		echo "Must run from $ISTIO"
 		return 1
 	fi
-  tools/docker --targets=pilot,proxyv2,app,install-cni,ztunnel --hub=$HUB --tag=$TAG --push
+  tools/docker --targets=pilot,proxyv2,app,install-cni,ztunnel --push --builder crane --hub=$HUB --tag=$TAG --variants distroless
 }
 
-function istioctl_install() {
+function istio_build_docker() {
 	if [ "$(pwd)" != "$ISTIO" ]; then
 		echo "Must run from $ISTIO"
 		return 1
 	fi
-	go run ./istioctl/cmd/istioctl install -y --manifests manifests --set hub=$HUB --set tag=$TAG $@
+  tools/docker --targets=pilot,proxyv2,app,install-cni,ztunnel --hub=$HUB --tag=$TAG --variants distroless
 }
 
-function istioctl_install_ambient() {
-	if [ "$(pwd)" != "$ISTIO" ]; then
-		echo "Must run from $ISTIO"
-		return 1
-	fi
-  go run ./istioctl/cmd/istioctl install -y --manifests manifests --set hub=$HUB --set tag=$TAG --set profile=ambient $@ 
-}
-
-function split_bug_report() {
+function istio_split_bug_report() {
   for n in nsdumps_cr nsdumps; do
     rm -rf $n; mkdir $n
   done
@@ -47,11 +37,6 @@ function split_bug_report() {
   cat k8s-resources | yq '.items | groupby(.metadata.namespace) | .[]' -s '"nsdumps/" + .[0].metadata.namespace + ".ns.yaml"'
 }
 
-function proxy_build() {
-  DEFAULT_PROXY_DIR=$(realpath $ISTIO/../proxy)
-  PROXY_DIR="${PROXY_DIR:-$DEFAULT_PROXY_DIR}"
-  docker start istio-proxy-build && docker attach istio-proxy-build || docker run --name istio-proxy-build -it -w /work -v $PROXY_DIR:/work gcr.io/istio-testing/build-tools-proxy:master-latest bash
-}
 
 function kfp() {
   LABEL=$1
